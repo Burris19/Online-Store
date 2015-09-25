1dd91e754eb3af6bd7a5f85aec662b05933cec7c
Antonio1394 <antoniorivm1394@gmail.com>
2015-09-25 16:41:12 -0600

    [add] agregando migraciones

--- database/migrations/2015_09_25_214100_create_provider_table.php
+++ database/migrations/2015_09_25_214100_create_provider_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCompanyTable extends Migration
+class CreateProviderTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,17 @@ class CreateCompanyTable extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('providers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('logo');
+            $table->string('code');
             $table->timestamps();
         });
+
     }
 
     /**
@@ -25,6 +32,6 @@ class CreateCompanyTable extends Migration
      */
     public function down()
     {
-        Schema::drop('companies');
+        Schema::drop('providers');
     }
 }
---
+++ database/migrations/2015_09_25_215548_create_producto_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_category')->unsigned();
+            $table->foreign('id_category')->references('id')->on('categories');
+            $table->string('title');
+            $table->string('description');
+            $table->string('code');
+            $table->decimal('price',18,2);
+            $table->string('image');
+            $table->integer('id_provider');
+            $table->foreign('id_provider')->references('id')->on('providers');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
