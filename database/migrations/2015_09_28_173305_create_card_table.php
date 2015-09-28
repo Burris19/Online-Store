a8fc7817f6499cf6fce24fc60fa59b731eb08e5d
Burris19 <julianhernandez19@gmail.com>
2015-09-28 11:40:27 -0600

    [add] migrations

---
+++ database/migrations/2015_09_28_173305_create_card_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('card_number');
+            $table->integer('last4');
+            $table->string('expiration_date');
+            $table->string('brand');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cards');
+    }
+}
