c27e10c44702940ad2c75672b5d20ceebb0af821
Burris19 <julianhernandez19@gmail.com>
2015-10-03 15:13:20 -0600

    [add] configure template base

---
+++ resources/views/admin/_base/home/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 1.0.1
+  </div>
+  <strong>Copyright &copy;2015-2016 <a href="#">Simulacion y Modelacion </a>.</strong> All rights reserved.
+</footer>
