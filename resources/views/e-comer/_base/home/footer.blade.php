001fb3cebeb86f554963ea3c9dcc25f978662e66
Burris19 <julianhernandez19@gmail.com>
2015-10-12 15:25:35 -0600

    [update] view index client

--- resources/views/e-comer/_base/home/footer.blade.php
+++ resources/views/e-comer/_base/home/footer.blade.php
@@ -1,13 +1,10 @@
 <div id="copyright">
     <div class="container">
         <div class="col-md-6">
-            <p class="pull-left">© 2015 Your name goes here.</p>
+            <p class="pull-left">© 2015 Simulacion y modelacion.</p>
 
         </div>
-        <div class="col-md-6">
-            <p class="pull-right">Template by <a href="http://www.bootstrapious.com">Responsive Templates</a> with support from <a href="http://kakusei.cz">Designové předměty</a>
-                <!-- Not removing these links is part of the licence conditions of the template. Thanks for understanding :) -->
-            </p>
+        <div class="col-md-6">            
         </div>
     </div>
 </div>
