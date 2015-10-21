22f97d5f2805dcfc823b193634783301ff8beffc
Burris19 <julianhernandez19@gmail.com>
2015-10-21 12:44:24 -0600

    [update] folder imagen

--- resources/views/e-comer/_base/home/carusel.blade.php
+++ resources/views/e-comer/_base/home/carusel.blade.php
@@ -2,13 +2,13 @@
     <div class="col-md-12">
         <div id="main-slider">
             <div class="item">
-                <img src="img/uno.jpg" alt="" class="img-responsive">
+                <img src="baner/uno.jpg" alt="" class="img-responsive">
             </div>
             <div class="item">
-                <img class="img-responsive" src="img/tres.jpg" alt="">
+                <img class="baner-responsive" src="img/tres.jpg" alt="">
             </div>
             <div class="item">
-                <img class="img-responsive" src="img/cuatro.jpg" alt="">
+                <img class="baner-responsive" src="img/cuatro.jpg" alt="">
             </div>
         </div>
         <!-- /#main-slider -->
